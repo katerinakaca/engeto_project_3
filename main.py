3dd7ad9c8200f2907e08cdf657165b0fe2ed1915
katerinakaca <urbancova.kata@seznam.cz>
2022-03-23 14:14:55 +0100

    zpracovani odkazu

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
+import csv
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    # odkaz_na_kod = "https://volby.cz/pls/ps2017nss/ps32?xjazyk=CZ&xkraj=7&xnumnuts=5101"
+    nazev_obce = "Doksy"
+    soubor_csv = "vysledky_doksy.csv"
+    odkaz = "https://volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj=7&xobec=561495&xvyber=5101"
+    html = requests.get(odkaz)
+    soup = BeautifulSoup(html.text, "html.parser")
+    tabulka_1 = soup.find_all("td", {"class": "cislo"})
+    tabulka_2 = soup.find_all("td", {"class": "overflow_name"})
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def data_pro_csv(tabulka_1, tabulka_2):
+    
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# def najit_kod_obce(odkaz_na_kod, nazev_obce):
+#     kod = requests.get(odkaz_na_kod)
+#     soup = BeautifulSoup(kod.text, "html.parser")
+#     vsechny_td = soup.find_all("td", {"class": "overflow_name"})
+#     spravny_tag = []
+#     print(vsechny_td)
+#     # for tr in vsechny_tr:
