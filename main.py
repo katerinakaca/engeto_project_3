af4786a97abdb60fab73b8645d8d4f46b7ade89c
katerinakaca <urbancova.kata@seznam.cz>
2022-03-24 08:03:02 +0100

    zpracovani odkazu ziskani dat o volicich kandidujici strany

--- main.py
+++ main.py
@@ -11,13 +11,25 @@ html = requests.get(odkaz)
 soup = BeautifulSoup(html.text, "html.parser")
 tabulka_1 = soup.find("table", {"id": "ps311_t1"})
 list_1 = tabulka_1.find_all("td", {"class": "cislo"})
-tabulka_2 = soup.find_all("td", {"class": "overflow_name"})
+tabulka_2 = soup.find_all("td", {"class": "overflow_name","headers":"t1sa1 t1sb2"})
+# list_2 = tabulka_2.get_text()
+tabulka_mesto = soup.find("div", {"id": "publikace", "class": "topline"})
+mesto_list = tabulka_mesto.find_all("h3")
 volici_v_seznamu = list_1[3].get_text()
 vydane_obalky = list_1[4].get_text()
 platne_hlasy = list_1[7].get_text()
+mesto = (mesto_list[2].get_text())[7:]
+kod_mesta = odkaz[-18:-12]
+strany = []
+
+for strana in tabulka_2[:]:
+    kand_strana = strana.get_text()
+    strany.append(kand_strana)
 
 
-print(platne_hlasy)
+
+
+print(strany)
 
 
 
